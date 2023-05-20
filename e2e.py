2495cdaf2c2721329ed4348dc9beac53539c00a7
nir nimova <nir.nimova@aiosystems.com>
2023-05-20 16:51:39 +0300

    yalla yalla

--- e2e.py
+++ e2e.py
@@ -1,10 +1,12 @@
 # coding=UTF8
 
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # 1
 chrome_driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+print(os.environ['PATH'])
 
 
 def main_function():
