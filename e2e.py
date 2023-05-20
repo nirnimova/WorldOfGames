5cf8374976c011b6c929e55951f014c275190484
nir nimova <nir.nimova@aiosystems.com>
2023-05-20 16:00:17 +0300

    yalla yalla

--- e2e.py
+++ e2e.py
@@ -1,5 +1,14 @@
+# coding=UTF8
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 1
+chrome_driver = webdriver.Chrome('/usr/local/bin/chromedriver.exe')
+
+
 def main_function():
-    print('TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    chrome_driver.get('https://www.walla.co.il')
 
 
 main_function()
