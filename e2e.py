596470386da9abba95c152286e607a8a16005db3
nir nimova <nir.nimova@aiosystems.com>
2023-05-20 17:59:10 +0300

    yalla yalla

--- e2e.py
+++ e2e.py
@@ -1,15 +1,25 @@
 # coding=UTF8
 
-import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-print(os.environ['PATH'])
-chrome_driver = webdriver.Chrome(executable_path='chromedriver.exe')
+
+def test_scores_service(url):
+    chrome_driver = webdriver.Chrome(executable_path='chromedriver.exe')
+    chrome_driver.get(url)
+    score = chrome_driver.find_element(By.ID, "score").text
+    if 1 <= int(score) <= 50:
+        return True
+    else:
+        return False
 
 
 def main_function():
-    chrome_driver.get('https://www.walla.co.il')
+    if test_scores_service('http://localhost:8777/'):
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 
 main_function()
